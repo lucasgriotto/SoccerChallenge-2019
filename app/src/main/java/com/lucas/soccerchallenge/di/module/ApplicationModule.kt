808422723417d5982f742535276389ff929284a7
Lucas Griotto <lucasgriotto@hotmail.com>
2023-03-10 13:20:10 -0300

    Dispatcher is now injected on the repository

--- app/src/main/java/com/lucas/soccerchallenge/di/module/ApplicationModule.kt
+++ app/src/main/java/com/lucas/soccerchallenge/di/module/ApplicationModule.kt
@@ -2,10 +2,15 @@ package com.lucas.soccerchallenge.di.module
 
 import android.app.Application
 import android.content.Context
+import com.lucas.soccerchallenge.di.qualifier.DefaultDispatcher
+import com.lucas.soccerchallenge.di.qualifier.IODispatcher
 import com.lucas.soccerchallenge.repository.SoccerRepository
 import com.lucas.soccerchallenge.repository.SoccerRepositoryImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
 @Module
 @Suppress("UNUSED")
@@ -16,5 +21,17 @@ abstract class ApplicationModule {
 
     @Binds
     abstract fun bindSoccerRepository(repository: SoccerRepositoryImpl): SoccerRepository
+
+
+    companion object {
+
+        @Provides
+        @DefaultDispatcher
+        fun defaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+        @Provides
+        @IODispatcher
+        fun ioDispatcher(): CoroutineDispatcher = Dispatchers.IO
+    }
 
 }
--- app/src/main/java/com/lucas/soccerchallenge/repository/SoccerRepositoryImpl.kt
+++ app/src/main/java/com/lucas/soccerchallenge/repository/SoccerRepositoryImpl.kt
@@ -4,6 +4,9 @@ import com.lucas.soccerchallenge.api.SoccerService
 import com.lucas.soccerchallenge.core.extension.safeApiCall
 import com.lucas.soccerchallenge.data.Match
 import com.lucas.soccerchallenge.data.toMatch
+import com.lucas.soccerchallenge.di.qualifier.DefaultDispatcher
+import com.lucas.soccerchallenge.di.qualifier.IODispatcher
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -11,19 +14,20 @@ import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class SoccerRepositoryImpl @Inject constructor(
-    private val service: SoccerService
+    private val service: SoccerService,
+    @DefaultDispatcher private val dispatcher: CoroutineDispatcher
 ) : SoccerRepository {
 
     override fun fetchFixture(): Flow<List<Match>> = flow {
         val fixture = safeApiCall { service.getFixture() }
             .map { it.toMatch() }
         emit(fixture)
-    }.flowOn(Dispatchers.Default)
+    }.flowOn(dispatcher)
 
     override fun fetchMatchResults(): Flow<List<Match>> = flow {
         val results = safeApiCall { service.getResults() }
             .map { it.toMatch() }
         emit(results)
-    }.flowOn(Dispatchers.Default)
+    }.flowOn(dispatcher)
 
 }
